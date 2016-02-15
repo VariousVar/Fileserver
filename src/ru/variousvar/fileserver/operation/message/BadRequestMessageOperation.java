ef1eac49d18e199ca8fe16b9f721f726412e7560
Anatoly Sibiryatko <a.sibiryatko@gmail.com>
2016-02-16 00:45:06 +0300

    BadRequest message implemented.

--- src/ru/variousvar/fileserver/operation/message/BadRequestMessageOperation.java
+++ src/ru/variousvar/fileserver/operation/message/BadRequestMessageOperation.java
@@ -1,10 +1,11 @@
 package ru.variousvar.fileserver.operation.message;
 
 import ru.variousvar.fileserver.message.Message;
+import ru.variousvar.fileserver.message.MessageType;
 
 public class BadRequestMessageOperation implements MessageOperation {
 	@Override
 	public Message createResponse(Message originalMessage) {
-		return null;
+		return new Message(MessageType.BAD_REQUEST);
 	}
 }
