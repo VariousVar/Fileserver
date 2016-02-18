b1b19a73d1b4bd018bc49a87321ebb4ab7109bf2
Anatoly Sibiryatko <a.sibiryatko@gmail.com>
2016-02-18 16:01:46 +0300

    Add utils.

---
+++ src/ru/variousvar/ConsoleHelper.java
@@ -0,0 +1,27 @@
+package ru.variousvar;
+
+import java.util.Scanner;
+
+public class ConsoleHelper {
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void writeMessage(String message) {
+		System.out.println(message);
+	}
+
+	public static String readString() {
+		return scanner.nextLine();
+	}
+
+	public static int readInt() {
+		String text;
+
+		while (true) {
+			text = readString();
+
+			if (text.trim().matches("\\d+")) break;
+		}
+
+		return Integer.parseInt(text.trim());
+	}
+}
